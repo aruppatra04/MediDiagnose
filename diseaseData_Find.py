a2eed4576b9f01abec0cbc7112053396eb8b2960
Arup Patra <aruppatra2004@gmail.com>
2025-04-01 03:57:08 +0530

    .gitignore file updated

---
+++ diseaseData_Find.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+symtoms = pd.read_csv("datasets/symtoms.csv")
+precautions = pd.read_csv("datasets/precautions.csv")
+workout = pd.read_csv("datasets/workout.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv("datasets/medications.csv")
+diets = pd.read_csv("datasets/diets.csv")
+
+# custome and helping functions :: 
+
+def helper(dis):
+    desc = description[description['Disease'] == dis]['Description']
+    desc = " ".join([w for w in desc])
+
+    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
+    pre = [col for col in pre.values]
+
+    med = medications[medications['Disease'] == dis]['Medication']
+    med = [med for med in med.values]
+
+    die = diets[diets['Disease'] == dis]['Diet']
+    die = [die for die in die.values]
+
+    wrkout = workout[workout['disease'] == dis] ['workout']
+
+    return desc, pre, med, die, wrkout
